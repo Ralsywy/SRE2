038952160a0ac7be32405cbc33606a092053a53c
ralsy <bichonkevin4@gmail.com>
2024-01-09 11:58:42 +0100

    login

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -1,11 +1,11 @@
 @extends('base')
 
 @section('content')
-<x-guest-layout>
     <!-- Session Status -->
     <x-auth-session-status class="" :status="session('status')" />
 
     <div class="wrapper">
+        <h1>Formulaire de connexion</h1>
     <form method="POST" action="{{ route('login') }}">
         @csrf
 
@@ -29,16 +29,15 @@
 
         <!-- Remember Me -->
         <div class="remember-forgot">
-            <label for="remember_me" class="inline-flex items-center">
-                <input id="remember_me" type="checkbox" class="rounded border-gray-300 text-indigo-600 shadow-sm focus:ring-indigo-500" name="remember">
-                <span class="ms-2 text-sm text-gray-600">{{ __('Remember me') }}</span>
+            <label for="remember_me" class="">
+                <input id="remember_me" type="checkbox" class="" name="remember">
+                <span class="">Se souvenir</span>
             </label>
         </div>
-            <button type="submit" class="btn">
+            <button type="submit" class="btn1">
                 Se connecter
             </button>
         </div>
     </form>
 </div>
-</x-guest-layout>
 @endsection
