a8bfc4b77c019cc461c8a907407ecbd132213d91
ralsy <bichonkevin4@gmail.com>
2024-01-10 16:21:05 +0100

    inscrit css

---
+++ public/js/app.js
@@ -0,0 +1,8 @@
+function hideshowrdc(val) {
+    if (val == 1) {
+        document.getElementById('div_rdc').style.display = 'inline-block';
+    }
+    if (val == 2) {
+        document.getElementById('div_rdc').style.display = 'none';
+    }
+}
