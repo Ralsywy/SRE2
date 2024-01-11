2fcb91ca2fd5526546c9a57d1fae1d0169bf1d94
Sommaly <vanh.sommaly@gmail.com>
2024-01-11 16:25:57 +0100

    liste des inscrits

--- resources/views/voir-inscrits.blade.php
+++ resources/views/voir-inscrits.blade.php
@@ -1,6 +1,27 @@
 @extends('base')
-
-
 @section('content')
-
+<div class="page-inscrit">
+    <table>
+        <thead>
+            <tr>
+                <th>Nom</th>
+                <th>Prénom</th>
+                <th>Accompagnateur</th>
+                <th>Nombre de rendez-vous</th>
+                <th>Actions</th>
+            </tr>
+        </thead>
+        <tbody>
+            @foreach($inscrits as $ligne)
+            <tr>
+                <td>{{$ligne->nom}}</td>
+                <td>{{$ligne->prenom}}</td>
+                <td>{{$ligne->nom}}</td>
+                <td>{{$ligne->nb_rdv}}</td>
+                <td>{{$ligne->nom}}</td>
+            </tr>
+            @endforeach
+        </tbody>
+    </table>
+</div>
 @endsection
